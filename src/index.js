ea5f267a60b77ab34dbfabdcd200fbf9ead61e4a
somya <somushrestha448@gmail.com>
2025-01-26 18:23:17 +0530

    routing-init

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 const express = require("express")
 const {PORT} = require("./config")
+const apiRoutes = require("./routes")
 
 const app = express();
 
+app.use('/api', apiRoutes);
+
 app.listen(PORT, ()=>{
     console.log(`Sucessfully started the server on PORT : ${PORT}`)
-})
+}) 
---
+++ src/routes/v1/index.js
@@ -0,0 +1,11 @@
+const express = require("express")
+
+const router = express.Router();
+
+router.get("/info", (req,res)=>{
+    return res.json({
+        msg : "ok",
+    });
+})
+
+module.exports = router;
